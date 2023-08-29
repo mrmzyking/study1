1841fec516eb638e6fe5bbaf02dc1ae99dd4b128
mrmzyking <zhenyang.mo@gmail.com>
2023-08-29 17:09:18 +0800

    人工智能方面的学习计划V1.2

--- study2/test.py
+++ study2/test.py
@@ -1,5 +1,7 @@
 # coding: utf-8
 import sys, os
+from datetime import datetime
+
 sys.path.append(os.pardir)  # 为了导入父目录的文件而进行的设定
 import numpy as np
 import pickle
@@ -16,6 +18,7 @@ def init_network():
         network = pickle.load(f)
     return network
 
+
 def softmax(a):
     c = np.max(a)
     exp_a = np.exp(a - c)  # 溢出对策
@@ -23,8 +26,11 @@ def softmax(a):
     y = exp_a / sum_exp_a
     return y
 
-def sigmoid( x: np):
+
+def sigmoid(x: np):
     return 1 / (1 + np.exp(-x))
+
+
 def predict(network, x):
     W1, W2, W3 = network['W1'], network['W2'], network['W3']
     b1, b2, b3 = network['b1'], network['b2'], network['b3']
@@ -39,13 +45,42 @@ def predict(network, x):
     return y
 
 
-x, t = get_data()
-network = init_network()
-accuracy_cnt = 0
-for i in range(len(x)):
-    y = predict(network, x[i])
-    p= np.argmax(y) # 获取概率最高的元素的索引
-    if p == t[i]:
-        accuracy_cnt += 1
+def mean_squared_error(y, t):
+    return 0.5 * np.sum((y - t) ** 2)
 
-print("Accuracy:" + str(float(accuracy_cnt) / len(x)))
+
+def cross_entropy_error(y, t):
+    delta = 1e-7
+    return -np.sum(t * np.log(y + delta)) / y.shape[0]
+
+
+def cross_entropy_error1(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+    # y = y.reshape(1, y.size)
+    batch_size = y.shape[0]
+    return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
+
+
+(x_train, t_train), (x_test, t_test) = load_mnist(normalize=True, flatten=True, one_hot_label=True)
+train_size = x_train.shape[0]
+network = init_network()
+batch_size = 10
+batch_mask = np.random.choice(train_size, batch_size)
+x_batch = x_train[batch_mask]
+t_batch = t_train[batch_mask]
+y = predict(network, x_batch)
+loss = cross_entropy_error(y, t_batch)
+print("predict = {} \r\n t_batch = {} \r\n loss = {}".format(y, t_batch, loss))
+# x, t = get_data()
+# network = init_network()
+# batch_size = 5  # 批数量
+# accuracy_cnt = 0
+# for i in range(0, len(x), batch_size):
+#     x_batch = x[i:i + batch_size]
+#     y_batch = predict(network, x_batch)
+#     p = np.argmax(y_batch, axis=1)
+#     print("{} info : y = {} , p = {}".format(datetime.now(),y_batch,p))
+#     accuracy_cnt += np.sum(p == t[i:i + batch_size])
+#
+# print("Accuracy:" + str(float(accuracy_cnt) / len(x)))
---
+++ study2/two_layer_net.py
@@ -0,0 +1,118 @@
+# coding: utf-8
+import sys, os
+from datetime import datetime
+
+import numpy as np
+from dataset.mnist import load_mnist
+sys.path.append(os.pardir)  # 親ディレクトリのファイルをインポートするための設定
+from common.functions import *
+from common.gradient import numerical_gradient
+import numpy as np
+
+
+class TwoLayerNet:
+
+    def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):
+        # 重みの初期化
+        self.params = {}
+        self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
+        self.params['b1'] = np.zeros(hidden_size)
+        self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size)
+        self.params['b2'] = np.zeros(output_size)
+
+    def predict(self, x):
+        W1, W2 = self.params['W1'], self.params['W2']
+        b1, b2 = self.params['b1'], self.params['b2']
+
+        a1 = np.dot(x, W1) + b1
+        z1 = sigmoid(a1)
+        a2 = np.dot(z1, W2) + b2
+        y = softmax(a2)
+
+        return y
+
+    # x:入力データ, t:教師データ
+    def loss(self, x, t):
+        y = self.predict(x)
+        return cross_entropy_error(y, t)
+
+    def accuracy(self, x, t):
+        y = self.predict(x)
+        y = np.argmax(y, axis=1)
+        t = np.argmax(t, axis=1)
+
+        accuracy = np.sum(y == t) / float(x.shape[0])
+        return accuracy
+
+    # x:入力データ, t:教師データ
+    def numerical_gradient(self, x, t):
+        loss_W = lambda W: self.loss(x, t)
+        grads = {}
+        grads['W1'] = numerical_gradient(loss_W, self.params['W1'])
+        grads['b1'] = numerical_gradient(loss_W, self.params['b1'])
+        grads['W2'] = numerical_gradient(loss_W, self.params['W2'])
+        grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
+
+        return grads
+
+    def gradient(self, x, t):
+        W1, W2 = self.params['W1'], self.params['W2']
+        b1, b2 = self.params['b1'], self.params['b2']
+        grads = {}
+
+        batch_num = x.shape[0]
+
+        # forward
+        a1 = np.dot(x, W1) + b1
+        z1 = sigmoid(a1)
+        a2 = np.dot(z1, W2) + b2
+        y = softmax(a2)
+
+        # backward
+        dy = (y - t) / batch_num
+        grads['W2'] = np.dot(z1.T, dy)
+        grads['b2'] = np.sum(dy, axis=0)
+
+        dz1 = np.dot(dy, W2.T)
+        da1 = sigmoid_grad(a1) * dz1
+        grads['W1'] = np.dot(x.T, da1)
+        grads['b1'] = np.sum(da1, axis=0)
+
+        return grads
+
+if __name__ == '__main__':
+    (x_train, t_train), (x_test, t_test) =  load_mnist(normalize=True, one_hot_label = True)
+    train_loss_list = []
+    train_acc_list = []
+    test_acc_list = []
+    # 平均每个epoch的重复次数
+    # 超参数
+    iters_num = 50000
+    train_size = x_train.shape[0]
+    batch_size = 100
+    learning_rate = 0.01
+    iter_per_epoch = max(train_size / batch_size, 1)
+    network = TwoLayerNet(input_size=784, hidden_size=50, output_size=10,weight_init_std=learning_rate)
+    for i in range(iters_num):
+        # 获取mini-batch
+        batch_mask = np.random.choice(train_size, batch_size)
+        x_batch = x_train[batch_mask]
+        t_batch = t_train[batch_mask]
+        # 计算梯度
+        # grad = network.numerical_gradient(x_batch, t_batch)
+        grad = network.gradient(x_batch, t_batch) # 高速版!
+        # 更新参数
+        for key in ('W1', 'b1', 'W2', 'b2'):
+            network.params[key] -= learning_rate * grad[key]
+        # 记录学习过程
+        loss = network.loss(x_batch, t_batch)
+        # print("{} loss = {} ".format(datetime.now(),loss))
+        train_loss_list.append(loss)
+        # 计算每个epoch的识别精度
+        if i % iter_per_epoch == 0:
+            train_acc = network.accuracy(x_train, t_train)
+            test_acc = network.accuracy(x_test, t_test)
+            train_acc_list.append(train_acc)
+            test_acc_list.append(test_acc)
+            print("train acc, test acc | " + str(train_acc) + ", " + str(test_acc))
+    # print(train_loss_list)
